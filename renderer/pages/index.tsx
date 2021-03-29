4b05fe9802d26f404b8e8b9aed4aa17fe26f860e
shashisrinath <atshashisrinath@gmail.com>
2021-03-29 23:47:47 +0530

    removed extra content on homepage

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,42 +1,15 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
-import Layout from '../layouts/Layout';
-
-import dynamic from 'next/dynamic';
-
-const ReactAdmin = dynamic(
-    () => import('../components/react-admin/ReactAdmin'),
-    {
-        ssr: false,
-    }
-);
-
-const useStyles = makeStyles((_theme: Theme) =>
-    createStyles({
-        titleContainer: {
-            display: 'flex',
-            justifyContent: 'center',
-            paddingTop: 40,
-        },
-        buttonContainer: {
-            paddingTop: 60,
-            position: 'absolute',
-            left: '50%',
-            top: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: '100%',
-            paddingLeft: '30px',
-            paddingRight: '30px',
-        },
-    })
-);
+import BasePageLayout from '../layouts/BasePageLayout';
+import Link from 'next/link';
 
 const IndexPage = () => {
-    const classes = useStyles();
-
     return (
-        <Layout title="Dashboard">
-            <ReactAdmin />
-        </Layout>
+        <BasePageLayout title={'Dashboard'}>
+            <div>
+                <Link href={'/orders'}>
+                    <a>Orders</a>
+                </Link>
+            </div>
+        </BasePageLayout>
     );
 };
 
